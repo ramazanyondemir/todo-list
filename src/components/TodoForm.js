b787a9fdfd86714c590ba7b4248dbc1af39c8ba1
Ramazan Yondemir <ramazan.yondemir@hotmail.com>
2022-02-19 21:25:29 +0300

    simple mobile optimization

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 import { AiOutlinePlus } from "react-icons/ai";
 
 const TodoForm = ({ todos, setTodos, todoValue, setTodoValue }) => {
